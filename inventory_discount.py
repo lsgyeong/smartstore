44a9383ea0b94b041f7e0704dcb68f6f4651bc6a
lsgyeong <tjd12rud12@naver.com>
2023-01-17 14:42:32 +0900

    재고관리: 각 밀키트 제조시 재고 수량 감소

--- inventory_discount.py
+++ inventory_discount.py
@@ -15,48 +15,24 @@ class WindowClass(QMainWindow, form_class) :
 
         self.stackedWidget.setCurrentIndex(5)
 
-        self.tteokbokki1_produce_btn.clicked.connect(lambda :self.discount(0))
-        self.tteokbokki2_produce_btn.clicked.connect(lambda :self.discount(1))
-        self.pasta_produce_btn.clicked.connect(lambda: self.discount(2))
-        self.soba_produce_btn.clicked.connect(lambda: self.discount(3))
-        self.kimchi_produce_btn.clicked.connect(lambda: self.discount(4))
-        self.softtofu_produce_btn.clicked.connect(lambda: self.discount(5))
+        # 떡볶이, 로제떡볶이, 봉골레파스타, 야끼소바, 김치찌개, 순두부찌개 버튼 눌렀을 때 이동
+        self.tteokbokki1_produce_btn.clicked.connect(lambda :self.mealkit_discount(0))
+        self.tteokbokki2_produce_btn.clicked.connect(lambda :self.mealkit_discount(1))
+        self.pasta_produce_btn.clicked.connect(lambda: self.mealkit_discount(2))
+        self.soba_produce_btn.clicked.connect(lambda: self.mealkit_discount(3))
+        self.kimchi_produce_btn.clicked.connect(lambda: self.mealkit_discount(4))
+        self.softtofu_produce_btn.clicked.connect(lambda: self.mealkit_discount(5))
 
     # 재고관리에서 제조버튼을 누르면 self.discount 매서드 실행
     def mealkit_discount(self,num):
         self.discount(k=num)
 
-    # 각 밀키트별 콤보박스 수량 읽기 위한 메서드
-    def combo_count(self,k):
-        # 일반떡볶이
-        if k == 0 :
-            count=self.comboBox_4.currentText()
-            return count
-        # 로제떡볶이
-        elif k == 1 :
-            count=self.comboBox_3.currentText()
-            return count
-        # 봉골레파스타
-        elif k == 2 :
-            count=self.comboBox_5.currentText()
-            return count
-        # 야끼소바
-        elif k == 3 :
-            count=self.comboBox_6.currentText()
-            return count
-        # 김치찌개
-        elif k == 4 :
-            count=self.comboBox_7.currentText()
-            return count
-        # 순두부찌개
-        elif k == 5 :
-            count=self.comboBox_8.currentText()
-            return count
-
     # 밀키트 제조시 재고수량 감소시키기 위한 매서드
     def discount(self,k):
         mealkit_name=['떡볶이','로제떡볶이','봉골레파스타','아끼소바','김치찌개','순두부찌개']
-        count=self.combo_count(k)
+        combobox_list=[self.comboBox_4,self.comboBox_3,self.comboBox_5,self.comboBox_6,self.comboBox_7,self.comboBox_8]
+        count = combobox_list[k].currentText()
+        print(count)
 
         # 밀키트, 재료 DB 가져오기
         conn=pymysql.connect(host='127.0.0.1', port=3306, user='root', password='1234', db='mealkit')
@@ -69,6 +45,9 @@ class WindowClass(QMainWindow, form_class) :
         for i in range(len(self.discount_db)):
             temp=int(self.discount_db[i][12])-(int(self.discount_db[i][6])*int(count))
             inventory_list.append(temp)
+
+        QMessageBox.information(self,'알림','제조완료')
+        # 여기서 리스트화된 재고량 보고 최소수량이하면 알림띄우기
 
         # DB저장위해 recipe_code 리스트화
         code_list=[]
